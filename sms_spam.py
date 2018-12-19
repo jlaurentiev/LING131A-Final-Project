a9e85752a8acc9c208453741cfc53e2bb4e18e6f
jlaurentiev <42979712+jlaurentiev@users.noreply.github.com>
2018-12-19 12:04:20 -0500

    Confusion matrix
    Added function to generate confusion matrix

--- sms_spam.py
+++ sms_spam.py
@@ -33,20 +33,21 @@ def create_feature_sets(labeled_data):
     # create feature sets
     ham_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'ham']
     spam_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'spam']
-    feature_set = [(sms_features(inst, ham_bigrams, spam_bigrams), inst[0]) for inst in labeled_data]
+    # feature_set = [(sms_features(inst, ham_bigrams, spam_bigrams), inst[0]) for inst in labeled_data]
+    feature_set = [(sms_features(inst), inst[0]) for inst in labeled_data]
     div = int(len(labeled_data) * 0.1)
     train_set = feature_set[div:]
     dev_test_set = feature_set[:div]
     return train_set, dev_test_set
 
 
-def sms_features(instance, ham_bigrams, spam_bigrams):
+def sms_features(instance):
     message = instance[1]
     message_tokens = nltk.word_tokenize(message)
     message_tags = nltk.pos_tag(message_tokens)
     name_data = open(os.path.join(dir, 'Names.txt')).read().splitlines()
-    fdist_ham_bigrams = nltk.FreqDist(ham_bigrams)
-    fdist_spam_bigrams = nltk.FreqDist(spam_bigrams)
+    # fdist_ham_bigrams = nltk.FreqDist(ham_bigrams)
+    # fdist_spam_bigrams = nltk.FreqDist(spam_bigrams)
     message_bigrams = list(nltk.bigrams(message_tokens))
     
     def len_feature(text):
@@ -70,8 +71,8 @@ def sms_features(instance, ham_bigrams, spam_bigrams):
         'capitalization': re.findall(r'^[A-Z\s]+$', message) is not None,
         'find_website': re.findall(r'\A(http://)?(www)?.*\Z', message) is not None,
         'contains_name': [word for word in message_tokens if word.title() in name_data] != [],
-        'contains_common_ham_bigram': [bgram for bgram in message_bigrams if bgram in fdist_ham_bigrams.most_common(10)] != [],
-        'contains_common_spam_bigram': [bgram for bgram in message_bigrams if bgram in fdist_spam_bigrams.most_common(10)] != []
+        # 'contains_common_ham_bigram': [bgram for bgram in message_bigrams if bgram in fdist_ham_bigrams.most_common(10)] != [],
+        # 'contains_common_spam_bigram': [bgram for bgram in message_bigrams if bgram in fdist_spam_bigrams.most_common(10)] != []
         }
 
 
@@ -94,23 +95,38 @@ def evaluate_classifier(classifier, dev_test_set):
     # get the accuracy of the test set
     return str(nltk.classify.accuracy(classifier, dev_test_set))
 
+
 def run_classifier(classifier):
     labeled_data = load_labeled_data('SMS','test_set.csv')
-    ham_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'ham']
-    spam_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'spam']
-    test_set = [(sms_features(inst, ham_bigrams, spam_bigrams), inst[0]) for inst in labeled_data]
+    # ham_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'ham']
+    # spam_bigrams = [nltk.bigrams(text) for (label, text) in labeled_data if label == 'spam']
+    # test_set = [(sms_features(inst, ham_bigrams, spam_bigrams), inst[0]) for inst in labeled_data]
+    test_set = [(sms_features(inst), inst[0]) for inst in labeled_data]
     return evaluate_classifier(classifier, test_set)
+
+
+def confusion_matrix(classifier):
+    data_dir = os.path.join(dir, 'SMS')
+    filename = 'test_data.csv'
+    data = open(os.path.join(data_dir, filename), encoding="Latin-1").read().splitlines()
+    labeled_data = [(inst[:inst.find(',')], pre_process_data(inst[inst.find(',')+1:])) for inst in data]
+    gold = [label for (label, message) in labeled_data]
+    test = [classifier.classify(sms_features(inst)) for inst in labeled_data]
+    cm = nltk.ConfusionMatrix(gold, test)
+    print(cm.pretty_format(sort_by_count=True, show_percents=True, truncate=9))
+
 
 if __name__ == '__main__':
 
     labeled_data = load_labeled_data('SMS','development_set.csv')
     training_set, dev_test_set = create_feature_sets(labeled_data)
     classifier_bayes = train_classifier_bayes(training_set)
-    classifier_dec_tree = train_classifier_dec_tree(training_set)
-    classifier_max_ent = train_classifier_max_ent(training_set)
-    print(classifier_dec_tree.pretty_format())
-    nltk.NaiveBayesClassifier.train(training_set).show_most_informative_features(10)
-    print('Naive Bayes accuracy ' + evaluate_classifier(classifier_bayes, dev_test_set))
-    print('Decision Tree accuracy ' + evaluate_classifier(classifier_dec_tree, dev_test_set))
-    print('Maximum Entropy accuracy ' + evaluate_classifier(classifier_max_ent, dev_test_set))    
-    run_classifier(classifier)
+    confusion_matrix(classifier_bayes)
+    # classifier_dec_tree = train_classifier_dec_tree(training_set)
+    # classifier_max_ent = train_classifier_max_ent(training_set)
+    # print(classifier_dec_tree.pretty_format())
+    # nltk.NaiveBayesClassifier.train(training_set).show_most_informative_features(10)
+    # print('Naive Bayes accuracy ' + evaluate_classifier(classifier_bayes, dev_test_set))
+    # print('Decision Tree accuracy ' + evaluate_classifier(classifier_dec_tree, dev_test_set))
+    # print('Maximum Entropy accuracy ' + evaluate_classifier(classifier_max_ent, dev_test_set))    
+    # run_classifier(classifier)
